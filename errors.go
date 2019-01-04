c6f56d13e63460e2c8133ccc9ff5f94063bd6374
Ivan Bertona <ivan.bertona@gmail.com>
2019-01-04 17:44:54 -0500

    Add some documentation.

--- errors.go
+++ errors.go
@@ -5,12 +5,10 @@ import "strings"
 // Errors describes a compound error, i.e. an error composed by multiple errors. It can be created using Append(). It
 // can generally be consumed as any other error (or *Error), although it is subject to some special casings:
 //
-//  * Error() returns "multiple errors: " followed by the concatenation of all individual Error() calls.
-//  * On Wrap(), Behaviors are always applied to the last error.
-//  * Metadata getters such as GetPublicMessage() search starting from the last error and return the first match found.
-//  * Equals() matches against all of the errors, returns true if at least one match is found.
-//  * Unwrap() unwraps and returns the last error.
-//  * An empty Errors is treated as nil (e.g. in MaybeWrap()).
+// Error() returns "multiple errors: " followed by the concatenation of all individual Error() calls.  On Wrap(),
+// Behaviors are always applied to the last error. Metadata getters such as GetPublicMessage() search starting from the
+// last error and return the first match found. Equals() matches against all of the errors, returns true if at least one
+// match is found. Unwrap() unwraps and returns the last error. An empty Errors is treated as nil (e.g. in MaybeWrap()).
 type Errors []*Error
 
 // Error implements error. It returns "multiple errors: " followed by the concatenation of all individual Error() calls.
